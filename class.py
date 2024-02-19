eb73a97febe0b13305f8b601896162415fe3483c
Wangari-j <wangarijudy@gmail.com>
2024-02-19 06:04:48 +0300

    class

---
+++ class.py
@@ -0,0 +1,14 @@
+class Triangle:
+    base = 0
+    height = 0
+
+    def __init__(self,base, height):
+      self.base = base
+      self.height = height
+
+    def getArea:
+    result = self.base * self.height/2
+    return result
+
+t1 = Triangle(4,12)
+print(t1.getArea)
